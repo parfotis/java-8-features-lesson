5b2ab4a732a9ba8f8e31b1551d496ef596be8fe2
Giannis Kavvouras <giannis.kavvouras@camelotglobal.com>
2020-06-16 01:55:46 +0300

    simplifies player

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,11 +1,8 @@
 package model;
 
-import java.time.LocalDate;
-
 public class Person {
     private String name;
     private int age;
-    private LocalDate birthday;
     private Sex gender;
     private String emailAddress;
 
@@ -23,14 +20,6 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
     }
 
     public Sex getGender() {
