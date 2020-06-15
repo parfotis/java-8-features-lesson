adb6424c1fc1db6aa462ba19e4d744e81d4d1811
Giannis Kavvouras <giannis.kavvouras@camelotglobal.com>
2020-06-16 00:27:00 +0300

    adds counter example for average age

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String name;
+    private int age;
+    private LocalDate birthday;
+    private Sex gender;
+    private String emailAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public void setGender(Sex gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public enum Sex {
+        MALE,
+        FEMALE
+    }
+}
