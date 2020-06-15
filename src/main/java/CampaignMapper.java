776a15c73db521af9b860413f2cabf23913b2b41
Giannis Kavvouras <giannis.kavvouras@camelotglobal.com>
2020-06-16 02:04:48 +0300

    adds campaignMapper draft

---
+++ src/main/java/CampaignMapper.java
@@ -0,0 +1,11 @@
+import model.Person;
+import model.Transaction;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CampaignMapper {
+    public List<Person> getWeekIdleAccounts(List<Person> persons, List<Transaction> transactions) {
+        return Collections.emptyList();
+    }
+}
