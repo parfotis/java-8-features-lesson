6e11763d99b7a4f1f6626785cdb9367b3d0a3b9a
Giannis Kavvouras <giannis.kavvouras@camelotglobal.com>
2020-06-18 02:41:31 +0300

    adds exercise for CSVReportService

---
+++ src/main/java/CSVReportService.java
@@ -0,0 +1,29 @@
+import repositories.TransactionRepository;
+
+public class CSVReportService {
+
+    private final PersonsService personsService;
+    private final TransactionRepository transactionRepository;
+
+    public CSVReportService(PersonsService personsService, TransactionRepository transactionRepository) {
+        this.personsService = personsService;
+        this.transactionRepository = transactionRepository;
+    }
+
+    /**
+     * Retrieve the average consumption (transaction amount) per @{@link model.Person}'s distinct roles during the last month
+     *
+     * Note that roles are just tags that each person is assigned. ie 'student', 'gamer', 'athlete', 'parent'
+     * a Person may have multiple roles or none.
+     *
+     * @return data in csv file format,
+     *         where the first line depict the roles
+     *         and the second line the average consumption per role
+     * ie: (formatted example -- the actual output should be just comma separated)
+     * |student, gamer, parent|
+     * |10.50  , 20.10, 0     |
+     */
+    public String getAverageConsumptionPerRoleDuringTheLastMonth() {
+        throw new UnsupportedOperationException();
+    }
+}
