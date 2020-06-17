4d9bbc8c0306ce69f91634d486f8e6fa9ee1c471
Giannis Kavvouras <giannis.kavvouras@camelotglobal.com>
2020-06-18 00:26:53 +0300

    adds composite completable future

--- src/main/java/async/TransactionService.java
+++ src/main/java/async/TransactionService.java
@@ -4,8 +4,10 @@ import model.Transaction;
 import repositories.TransactionRepository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 public class TransactionService {
 
@@ -17,6 +19,16 @@ public class TransactionService {
 
     public Future<List<Transaction>> getTransactions() {
         return CompletableFuture.supplyAsync(transactionRepository::getTransactions);
+    }
+
+    public Future<Map<String, List<Transaction>>> getTransactionsPerEmail() {
+        return CompletableFuture.supplyAsync(transactionRepository::getTransactions)
+                .thenApply(this::groupByEmail);
+    }
+
+    private Map<String, List<Transaction>> groupByEmail(List<Transaction> transactions) {
+        return transactions.stream()
+                .collect(Collectors.groupingBy(Transaction::getEmailAddress));
     }
 
 
--- src/test/java/async/TransactionServiceTest.java
+++ src/test/java/async/TransactionServiceTest.java
@@ -2,11 +2,11 @@ package async;
 
 import model.Transaction;
 import org.junit.Test;
-import org.mockito.Mockito;
 import repositories.TransactionRepository;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -27,5 +27,22 @@ public class TransactionServiceTest {
         Future<List<Transaction>> future = transactionService.getTransactions();
 
         assertEquals(Collections.emptyList(), future.get());
+    }
+
+    @Test
+    public void shouldGetTransactionsByEmailAsychronously() throws ExecutionException, InterruptedException {
+        Transaction dummyTransaction = new Transaction();
+        dummyTransaction.setEmailAddress("john@test.com");
+        dummyTransaction.setAmount(10);
+
+        stub(transactionRepository.getTransactions())
+                .toReturn(Collections.singletonList(dummyTransaction));
+
+        Future<Map<String, List<Transaction>>> future = transactionService.getTransactionsPerEmail();
+
+        Map<String, List<Transaction>> expectedGrouping = future.get();
+
+        assertEquals(1, expectedGrouping.size());
+        assertEquals(Collections.singletonList(dummyTransaction), expectedGrouping.get("john@test.com"));
     }
 }
